0f7b998ff982ea6cb862dbb1b558c1583267273f
Tanjila Khanam <khanholy3@gmail.com>
2026-02-10 18:53:45 +0600

    Refactor menuAction.js by cleaning comments and adding close button
    Removed commented code and added a close button click action.

--- pageAction/menuAction.js
+++ pageAction/menuAction.js
@@ -9,28 +9,21 @@ export class MenuAction {
 
   async openMenu() {
     await this.menu.menuButton.click();
-    // Menu item visible houya mane holo menu animation sesh
     await this.menu.logout.waitFor({ state: 'visible' });
   }
 
   async resetAppState() {
     await this.openMenu();
-    
-    // SauceDemo side menu-te 'Element is outside of the viewport' error thik korar 
-    // shobcheye effective industry way holo dispatchEvent use kora (jodi animation thake)
     await this.menu.resetAppState.dispatchEvent('click');
     
-    // Close button locator
+ 
     const closeButton = this.page.locator('#react-burger-cross-btn');
     await closeButton.click();
-    
-    // Wait kora jate menu-ta purapuri bondho hoye jay
     await this.menu.menuButton.waitFor({ state: 'visible' });
   }
 
   async logout() {
     await this.openMenu();
-    // DispatchEvent use korle viewport-er error ashbe na
     await this.menu.logout.dispatchEvent('click');
   }
 }
